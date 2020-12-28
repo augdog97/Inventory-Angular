6b6f9f65ab12fda08cf7c8c230fa352b4d781541
augdog97 <augdog@me.com>
2020-12-28 12:31:51 -0500

    Added two way data binding to form-sku component

--- src/app/form-sku/form-sku.component.ts
+++ src/app/form-sku/form-sku.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-sku.component.css']
 })
 export class FormSkuComponent implements OnInit {
-
+sku: string;
   constructor() { }
 
   ngOnInit(): void {
